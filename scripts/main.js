16c8924231ed57440e10bf8630148deb0ca143d7
JuanHeis <49214999+JuanHeis@users.noreply.github.com>
2019-08-24 19:09:06 -0300

    Se comparan ambas fichas dando un resultado exitoso

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+var clicks = 0;
+var primerElemento;
+var segundoElemento;
+function flip(elemento) {
+    clicks++
+    
+    if (clicks==1) {
+        primerElemento = document.getElementById(elemento)
+        primerElemento.classList.remove('hide')
+        primerElemento.classList.add('flip-vertical-fwd', 'show')
+        console.log(primerElemento)
+    } else if (clicks==2) {
+        segundoElemento = document.getElementById(elemento)
+        segundoElemento.classList.remove('hide')
+        segundoElemento.classList.add('flip-vertical-fwd', 'show')
+        console.log(segundoElemento)
+        setTimeout(reIniciar, 500)
+    } else {}
+
+    
+    comparar()
+}
+function comparar(){
+    if (primerElemento!=null && segundoElemento!=null)
+        if (primerElemento.id != segundoElemento.id)
+            if (primerElemento.src == segundoElemento.src){
+                console.log('eureka!')
+            }
+}
+function parValido
+function reIniciar(){
+    clicks=0
+    primerElemento.classList.remove('flip-vertical-fwd', 'show')
+    primerElemento.classList.add('hide')
+    segundoElemento.classList.remove('flip-vertical-fwd', 'show')
+    segundoElemento.classList.add('hide')
+    primerElemento = null
+    segundoElemento = null
+}
