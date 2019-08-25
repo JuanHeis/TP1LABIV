ea471f75a84c404614729b489eeb101bfbc1495a
JuanHeis <49214999+JuanHeis@users.noreply.github.com>
2019-08-24 22:55:10 -0300

    huston Funciona el juego, con dos tipos de fichas distintos. parece no haber bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,73 @@
 var clicks = 0;
 var primerElemento;
 var segundoElemento;
+var elementosEncontrados = []
+
 function flip(elemento) {
     clicks++
-    
-    if (clicks==1) {
+    if (clicks == 1) {
         primerElemento = document.getElementById(elemento)
-        primerElemento.classList.remove('hide')
-        primerElemento.classList.add('flip-vertical-fwd', 'show')
-        console.log(primerElemento)
-    } else if (clicks==2) {
+        if (!buscarElemento(primerElemento)) {
+            primerElemento.classList.remove('hide')
+            primerElemento.classList.add('flip-vertical-fwd', 'show')
+            console.log(primerElemento)
+        }
+    } else if (clicks == 2) {
         segundoElemento = document.getElementById(elemento)
-        segundoElemento.classList.remove('hide')
-        segundoElemento.classList.add('flip-vertical-fwd', 'show')
-        console.log(segundoElemento)
-        setTimeout(reIniciar, 500)
+        if (!buscarElemento(segundoElemento)) {
+            segundoElemento.classList.remove('hide')
+            segundoElemento.classList.add('flip-vertical-fwd', 'show')
+            console.log(segundoElemento)
+            if (!comparar())
+                setTimeout(reIniciarAnimaciones, 500)
+            else reIniciarValores()
+        }
     } else {}
+}
 
-    
-    comparar()
+function buscarElemento(unElemento) {
+    let salida = false
+    elementosEncontrados.forEach(x => {
+        console.log('la wea')
+        console.log(x.id == unElemento.id)
+        if (x.id == unElemento.id)
+            salida = true
+    })
+    return salida
 }
-function comparar(){
-    if (primerElemento!=null && segundoElemento!=null)
+
+
+function comparar() {
+    if (primerElemento != null && segundoElemento != null)
         if (primerElemento.id != segundoElemento.id)
-            if (primerElemento.src == segundoElemento.src){
+            if (primerElemento.src == segundoElemento.src) {
+                parValido()
                 console.log('eureka!')
-            }
+                return true
+            } else return false
+    else return false
+    else return false
 }
-function parValido(){
-    segundoElemento.classList.remove('hide')
-    segundoElemento.classList.add('flip-vertical-fwd', 'show')
+
+function parValido() {
     primerElemento.classList.remove('hide')
     primerElemento.classList.add('flip-vertical-fwd', 'show')
+    segundoElemento.classList.remove('hide')
+    segundoElemento.classList.add('flip-vertical-fwd', 'show')
+    elementosEncontrados.push(primerElemento)
+    elementosEncontrados.push(segundoElemento)
 }
-function reIniciar(){
-    clicks=0
+
+function reIniciarValores() {
+    primerElemento = null
+    segundoElemento = null
+    clicks = 0
+}
+
+function reIniciarAnimaciones() {
     primerElemento.classList.remove('flip-vertical-fwd', 'show')
     primerElemento.classList.add('hide')
     segundoElemento.classList.remove('flip-vertical-fwd', 'show')
     segundoElemento.classList.add('hide')
-    primerElemento = null
-    segundoElemento = null
+    setTimeout(reIniciarValores, 500)
 }
