b5f5697bfdbcd0870e13ee37c7aef8c70ff737ac
JuanHeis <49214999+JuanHeis@users.noreply.github.com>
2019-08-25 18:53:27 -0300

    Esto funciona casi perfecto bugsbugsbugsbugs

--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,29 @@
-var fichas
+const MAX_FICHAS_POR_RENGLON = 6
+const RENGLON_1 = 1
+const RENGLON_2 = 2
+const RENGLON_3 = 3
+var idsFichaspar = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+var idsFichasimpar = [10, 11, 12, 13, 14, 15, 16, 17, 18]
+var urlsFichaspar = ["chrome.svg", //1
+    "facebook.svg", //2
+    "firefox.svg", //3
+    "google-icon.svg", //4
+    "html-5.svg", //5
+    "instagram-icon.svg", //6
+    "internetexplorer.svg", //7
+    "javascript.svg", //8
+    "opera.svg" //9
+]
+var urlsFichasimpar = ["chrome.svg", //1
+        "facebook.svg", //2
+        "firefox.svg", //3
+        "google-icon.svg", //4
+        "html-5.svg", //5
+        "instagram-icon.svg", //6
+        "internetexplorer.svg", //7
+        "javascript.svg", //8
+        "opera.svg" //9
+    ]
+    //1-11-1img/chrome.svg">
+    //
+var cuerpo = ["<div class='elementoJuego'><img id='", "' class='hide' onclick=flip('", "') src='img/", "'></div>"]
--- scripts/main.js
+++ scripts/main.js
@@ -2,27 +2,31 @@ var clicks = 0;
 var primerElemento;
 var segundoElemento;
 var elementosEncontrados = []
+var limitClick = false;
 
 function flip(elemento) {
-    clicks++
-    if (clicks == 1) {
-        primerElemento = document.getElementById(elemento)
-        if (!buscarElemento(primerElemento)) {
-            primerElemento.classList.remove('hide')
-            primerElemento.classList.add('flip-vertical-fwd', 'show')
-            console.log(primerElemento)
-        }
-    } else if (clicks == 2) {
-        segundoElemento = document.getElementById(elemento)
-        if (!buscarElemento(segundoElemento)) {
-            segundoElemento.classList.remove('hide')
-            segundoElemento.classList.add('flip-vertical-fwd', 'show')
-            console.log(segundoElemento)
-            if (!comparar())
-                setTimeout(reIniciarAnimaciones, 500)
-            else reIniciarValores()
-        }
-    } else {}
+    if (!limitClick) {
+        clicks++
+        if (clicks == 1) {
+            primerElemento = document.getElementById(elemento)
+            if (!buscarElemento(primerElemento)) {
+                primerElemento.classList.remove('hide')
+                primerElemento.classList.add('flip-vertical-fwd', 'show')
+                console.log(primerElemento)
+            }
+        } else if (clicks == 2) {
+            limitClick = true;
+            segundoElemento = document.getElementById(elemento)
+            if (!buscarElemento(segundoElemento)) {
+                segundoElemento.classList.remove('hide')
+                segundoElemento.classList.add('flip-vertical-fwd', 'show')
+                console.log(segundoElemento)
+                if (!comparar())
+                    setTimeout(reIniciarAnimaciones, 500)
+                else reIniciarValores()
+            }
+        } else { reIniciarValores() }
+    } else console.log("Dejai de hacer click po")
 }
 
 function buscarElemento(unElemento) {
@@ -62,6 +66,7 @@ function reIniciarValores() {
     primerElemento = null
     segundoElemento = null
     clicks = 0
+    limitClick = false;
 }
 
 function reIniciarAnimaciones() {
@@ -69,5 +74,65 @@ function reIniciarAnimaciones() {
     primerElemento.classList.add('hide')
     segundoElemento.classList.remove('flip-vertical-fwd', 'show')
     segundoElemento.classList.add('hide')
-    setTimeout(reIniciarValores, 500)
+    setTimeout(reIniciarValores, 100)
+}
+
+
+function comenzar() {
+    let contadorRenglon3 = 0,
+        contadorRenglon2 = 0,
+        contadorRenglon1 = 0
+    while (idsFichaspar.length != 0 && idsFichasimpar.length != 0) {
+        if (idsFichaspar.length != 0) {
+            let posFicha = parseInt(Math.random() * idsFichaspar.length)
+            let idRenglon = RENGLON_1
+            if (contadorRenglon1 < MAX_FICHAS_POR_RENGLON) {
+                contadorRenglon1++
+                idRenglon = RENGLON_1
+            } else if (contadorRenglon2 < MAX_FICHAS_POR_RENGLON) {
+                contadorRenglon2++
+                idRenglon = RENGLON_2
+            } else if (contadorRenglon3 < MAX_FICHAS_POR_RENGLON) {
+                contadorRenglon3++
+                idRenglon = RENGLON_3
+            }
+            let fichaID = idsFichaspar[posFicha]
+            let fichaURL = urlsFichaspar[posFicha]
+            let renglon = document.getElementById("renglon" + idRenglon)
+            let elemento = cuerpo[0] + fichaID + cuerpo[1] + fichaID + cuerpo[2] + fichaURL + cuerpo[3]
+            renglon.innerHTML += elemento
+            idsFichaspar = eliminarElemento(idsFichaspar, fichaID)
+            urlsFichaspar = eliminarElemento(urlsFichaspar, fichaURL)
+
+        }
+        if (idsFichasimpar.length != 0) {
+            console.log(idsFichasimpar.length)
+            let posFicha = parseInt(Math.random() * idsFichasimpar.length)
+            if (contadorRenglon1 < MAX_FICHAS_POR_RENGLON) {
+                contadorRenglon1++
+                idRenglon = RENGLON_1
+            } else if (contadorRenglon2 < MAX_FICHAS_POR_RENGLON) {
+                contadorRenglon2++
+                idRenglon = RENGLON_2
+            } else if (contadorRenglon3 < MAX_FICHAS_POR_RENGLON) {
+                contadorRenglon3++
+                idRenglon = RENGLON_3
+            }
+            console.log("Llegue")
+            let fichaID = idsFichasimpar[posFicha]
+            let fichaURL = urlsFichasimpar[posFicha]
+            let renglon = document.getElementById("renglon" + idRenglon)
+            let elemento = cuerpo[0] + fichaID + cuerpo[1] + fichaID + cuerpo[2] + fichaURL + cuerpo[3]
+            renglon.innerHTML += elemento
+            idsFichasimpar = eliminarElemento(idsFichasimpar, fichaID)
+            urlsFichasimpar = eliminarElemento(urlsFichasimpar, fichaURL)
+        } else console.log("no llegue")
+    }
+}
+
+function eliminarElemento(array, elemento) {
+    array = array.filter(e => {
+        return e !== elemento
+    })
+    return array
 }
